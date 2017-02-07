5d65db3d48bd79ac43c459e30890dedbc8f457de
sijos <ssny88@gmail.com>
2017-02-07 10:02:04 -0800

    add create and update instance methods for playwrights class

--- W3D2/plays_demo/plays.rb
+++ W3D2/plays_demo/plays.rb
@@ -92,4 +92,30 @@ class Playwright
     @name = options['name']
     @birth_year = options['birth_year']
   end
+
+  def create
+    raise "#{self} already in database" if @id
+    PlayDBConnection.instance.execute(<<-SQL, @name, @birth_year)
+    INSERT INTO
+      playwrights (name, birth_year)
+    VALUES
+      (?, ?)
+    SQL
+    @id = PlayDBConnection.instance.last_insert_row_id
+  end
+
+  def update
+    raise "#{self} not in database" unless @id
+    PlayDBConnection.instance.execute(<<-SQL, @name, @birth_year, @id)
+      UPDATE
+        playwrights
+      SET
+        name = ?, birth_year = ?
+      WHERE
+        ID = ?
+    SQL
+  end
+
+
+
 end
