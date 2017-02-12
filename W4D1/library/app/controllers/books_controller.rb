24d7279ba39d60ea7dde8cf7a730af24d5a69830
sijos <ssny88@gmail.com>
2017-02-12 11:55:15 -0800

    add completed library hw

--- W4D1/library/app/controllers/books_controller.rb
+++ W4D1/library/app/controllers/books_controller.rb
@@ -1,18 +1,24 @@
 class BooksController < ApplicationController
   def index
-    # your code here
+    @books = Book.all
+    render index: @books
   end
 
   def new
-    # your code here
+    render :new
   end
 
   def create
-    # your code here
+    book = Book.new(book_params)
+    book.save
+    redirect_to books_url
   end
 
   def destroy
-    # your code here
+    book = Book.find(params[:id])
+    book.destroy
+
+    redirect_to books_url
   end
 
   private
