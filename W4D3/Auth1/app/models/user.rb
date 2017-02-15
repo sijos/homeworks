aedb666d4dfe3f34f61bde3fa4e7d4e0c6eb872b
sijos <ssny88@gmail.com>
2017-02-15 00:51:58 -0800

    complete first auth hw in user.rb model

--- W4D3/Auth1/app/models/user.rb
+++ W4D3/Auth1/app/models/user.rb
@@ -1,2 +1,53 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  username        :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
+require 'securerandom'
+
 class User < ActiveRecord::Base
+  attr_reader :password
+
+  validates :username, :session_token, presence: true, uniqueness: true
+  validates :password_digest, presence: { message: "Password can't be blank" }
+  validates :password, length: { minimum: 6, allow_nil: true }
+
+  before_validation :ensure_session_token
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by_username(username)
+    return nil if user.nil?
+    if BCrypt::Password.new(user.password_digest).is_password?(password)
+      user
+    end
+    nil
+  end
+
+  def self.generate_session_token
+    SecureRandom.urlsafe_base64(16)
+  end
+
+  def reset_session_token!
+    self.session_token = User.generate_session_token
+    User.save!
+    self.session_token
+  end
+
+  def ensure_session_token
+    if session_token.nil?
+      self.session_token = User.generate_session_token
+    end
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(session_token)
+  end
 end
