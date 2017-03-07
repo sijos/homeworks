035eac9a60dcb1a8cd71fc0a601040305081a4c9
sijos <ssny88@gmail.com>
2017-03-07 12:31:05 -0800

    revise middleware logger to return properly

--- W7D2/solution_1/frontend/store/store.js
+++ W7D2/solution_1/frontend/store/store.js
@@ -2,7 +2,8 @@ import { createStore, applyMiddleware } from 'redux';
 import RootReducer from '../reducers/root_reducer';
 
 const configureStore = (preloadedState = {}) => {
-  const store = createStore(RootReducer, preloadedState, applyMiddleware(addLoggingToDispatch));
+  const store = createStore(RootReducer, preloadedState,
+    applyMiddleware(addLoggingToDispatch));
   store.subscribe(() => {
     localStorage.state = JSON.stringify(store.getState());
   });
@@ -15,6 +16,6 @@ const addLoggingToDispatch = store => next => action => {
   let dispatch = store.dispatch;
   console.log(store.getState());
   console.log(action);
-  dispatch(action);
+  next(action);
   console.log(store.getState());
 };
