5c6df0830f4a7d507c691f56d6043a0a5e724df1
sijos <ssny88@gmail.com>
2017-03-06 19:40:09 -0800

    complete calculator hw

--- W6D5/NPM/calculator.jsx
+++ W6D5/NPM/calculator.jsx
@@ -8,14 +8,79 @@ class Calculator extends React.Component {
       num1: "",
       num2: "",
     };
+    this.setNum1 = this.setNum1.bind(this);
+    this.setNum2 = this.setNum2.bind(this);
+    this.add = this.add.bind(this);
+    this.subtract = this.subtract.bind(this);
+    this.multiply = this.multiply.bind(this);
+    this.divide = this.divide.bind(this);
+    this.clear = this.clear.bind(this);
   }
 
+
   render() {
+    let { num1, num2, result } = this.state;
     return (
       <div>
-        <h1>{this.state.result}</h1>
+        <h1>{result}</h1>
+
+        <form>
+          <input type="text"
+            placeholder="first num"
+            onChange={this.setNum1}
+            value={num1}></input>
+          <input type="text"
+            placeholder="second num"
+            onChange={this.setNum2}
+            value={num2}></input>
+          <button onClick={this.clear}>Clear</button>
+        </form>
+        <button onClick={this.add}>+</button>
+        <button onClick={this.subtract}>-</button>
+        <button onClick={this.multiply}>*</button>
+        <button onClick={this.divide}>/</button>
       </div>
     );
+  }
+
+  clear(e) {
+    e.preventDefault();
+    this.setState({ num1: "", num2: "", result: 0 });
+  }
+
+  setNum1(e) {
+    e.preventDefault();
+    const num1 = e.target.value? parseInt(e.target.value) : "";
+    this.setState({ num1 });
+  }
+  setNum2(e) {
+    e.preventDefault();
+    const num2 = e.target.value? parseInt(e.target.value) : "";
+    this.setState({ num2 });
+  }
+
+  add(e) {
+    e.preventDefault();
+    const result = (this.state.num1 + this.state.num2);
+    this.setState({ result });
+  }
+
+  subtract(e) {
+    e.preventDefault();
+    const result = (this.state.num1 - this.state.num2);
+    this.setState({ result });
+  }
+
+  multiply(e) {
+    e.preventDefault();
+    const result = (this.state.num1 * this.state.num2);
+    this.setState({ result });
+  }
+
+  divide(e) {
+    e.preventDefault();
+    const result = (this.state.num1 / this.state.num2);
+    this.setState({ result });
   }
 }
 
