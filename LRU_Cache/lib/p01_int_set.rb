50de61358ed06e13a7ed22593726c20ed0edaa0a
Scott Snyder <ssny88@gmail.com>
2017-04-12 16:56:17 -0700

    complete p01 int set

--- LRU_Cache/lib/p01_int_set.rb
+++ LRU_Cache/lib/p01_int_set.rb
@@ -32,21 +32,26 @@ end
 class IntSet
   def initialize(num_buckets = 20)
     @store = Array.new(num_buckets) { Array.new }
+    @num_buckets = num_buckets
   end
 
   def insert(num)
+    self[num] << num
   end
 
   def remove(num)
+    self[num].delete(num)
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -63,18 +68,25 @@ class ResizingIntSet
   end
 
   def insert(num)
+    resize! if @count == num_buckets
+    self[num] << num
+    @count += 1
   end
 
   def remove(num)
+    self[num].delete(num)
+    @count -= 1
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -82,5 +94,11 @@ class ResizingIntSet
   end
 
   def resize!
+    new_buckets = num_buckets * 2
+    new_store = Array.new(new_buckets) { Array.new }
+    @store.each do |bucket|
+      bucket.each { |num| new_store[num % new_buckets] << num }
+    end
+    @store = new_store
   end
 end
