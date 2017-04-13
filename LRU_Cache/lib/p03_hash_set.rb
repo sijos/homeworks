f2def571bb734008cdd62a63e8e32e1dcbbc6f7f
Scott Snyder <ssny88@gmail.com>
2017-04-12 17:24:59 -0700

    complete p03 hash set

--- LRU_Cache/lib/p03_hash_set.rb
+++ LRU_Cache/lib/p03_hash_set.rb
@@ -9,18 +9,28 @@ class HashSet
   end
 
   def insert(key)
+    resize! if @count == num_buckets
+    hashed = key.hash
+    self[hashed] << hashed
+    @count += 1
   end
 
   def include?(key)
+    hashed = key.hash
+    self[hashed].include?(hashed)
   end
 
   def remove(key)
+    hashed = key.hash
+    self[hashed].delete(hashed)
+    @count -= 1
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -28,5 +38,13 @@ class HashSet
   end
 
   def resize!
+    old_store = @store
+    @count = 0
+    @store = Array.new(num_buckets * 2) { Array.new }
+    
+    old_store.flatten.each do |hashed|
+      self[hashed] << hashed
+      @count +=1
+    end
   end
 end
