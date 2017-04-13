5fb1ebfa0fdabafece8ed0c4130cca711856cd50
Scott Snyder <ssny88@gmail.com>
2017-04-12 17:07:06 -0700

    complete p02 hashing

--- LRU_Cache/lib/p02_hashing.rb
+++ LRU_Cache/lib/p02_hashing.rb
@@ -4,11 +4,17 @@ end
 
 class Array
   def hash
+    hash_num = 18
+    self.each_with_index do |el, i|
+      hash_num += el.hash * (i + 1)
+    end
+    hash_num
   end
 end
 
 class String
   def hash
+    self.chars.map(&:ord).hash
   end
 end
 
@@ -16,6 +22,6 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    self.keys.sort.concat(self.values.sort).hash
   end
 end
