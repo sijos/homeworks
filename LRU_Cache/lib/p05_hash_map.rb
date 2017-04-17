515b8626697f3f0bc12c3b6a3a6a60e5518ffbcb
Scott Snyder <ssny88@gmail.com>
2017-04-16 18:28:01 -0700

    complete p05 hash map

--- LRU_Cache/lib/p05_hash_map.rb
+++ LRU_Cache/lib/p05_hash_map.rb
@@ -19,6 +19,7 @@ class HashMap
     if buc.include?(key)
       buc.update(key, val)
     else
+      resize! if @count == num_buckets
       buc.append(key, val)
       @count += 1
     end
@@ -35,18 +36,18 @@ class HashMap
 
   def each(&prc)
     @store.each do |bucket|
-      bucket.each { yield [key, value] }
+      bucket.each { |link| yield [link.key, link.val] }
     end
     self
   end
 
   # uncomment when you have Enumerable included
-  # def to_s
-  #   pairs = inject([]) do |strs, (k, v)|
-  #     strs << "#{k.to_s} => #{v.to_s}"
-  #   end
-  #   "{\n" + pairs.join(",\n") + "\n}"
-  # end
+  def to_s
+    pairs = inject([]) do |strs, (k, v)|
+      strs << "#{k.to_s} => #{v.to_s}"
+    end
+    "{\n" + pairs.join(",\n") + "\n}"
+  end
 
   alias_method :[], :get
   alias_method :[]=, :set
@@ -58,6 +59,13 @@ class HashMap
   end
 
   def resize!
+    old_store = @store
+    @count = 0
+    @store = Array.new(num_buckets * 2) { LinkedList.new }
+
+    old_store.each do |bucket|
+      bucket.each { |link| set(link.key, link.val) }
+    end
   end
 
   def bucket(key)
