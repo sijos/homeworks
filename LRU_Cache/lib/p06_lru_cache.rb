839db1c69e499e2cefb9d99b16f82bd983318600
Scott Snyder <ssny88@gmail.com>
2017-04-16 19:26:06 -0700

    refactor LRU cache to improve time complexity

--- LRU_Cache/lib/p06_lru_cache.rb
+++ LRU_Cache/lib/p06_lru_cache.rb
@@ -16,11 +16,12 @@ class LRUCache
 
   def get(key)
     if @map.include?(key)
-      update_link!(@map[key])
+      link = @map[key]
+      update_link!(link)
+      link.val
     else
       calc!(key)
     end
-    @map[key].val
   end
 
   def to_s
@@ -32,9 +33,10 @@ class LRUCache
   def calc!(key)
     # suggested helper method; insert an (un-cached) key
     value = @prc.call(key)
-    @store.append(key, value)
-    @map.set(key, @store.last)
+    link = @store.append(key, value)
+    @map[key] = link
     eject! if count > @max
+    value
   end
 
   def update_link!(link)
