639741ee667d6ae279acfa40af743522c4076500
Scott Snyder <ssny88@gmail.com>
2017-04-16 19:16:52 -0700

    complete implementation of LRU cache

--- LRU_Cache/lib/p06_lru_cache.rb
+++ LRU_Cache/lib/p06_lru_cache.rb
@@ -15,6 +15,12 @@ class LRUCache
   end
 
   def get(key)
+    if @map.include?(key)
+      update_link!(@map[key])
+    else
+      calc!(key)
+    end
+    @map[key].val
   end
 
   def to_s
@@ -25,12 +31,21 @@ class LRUCache
 
   def calc!(key)
     # suggested helper method; insert an (un-cached) key
+    value = @prc.call(key)
+    @store.append(key, value)
+    @map.set(key, @store.last)
+    eject! if count > @max
   end
 
   def update_link!(link)
     # suggested helper method; move a link to the end of the list
+    link.remove
+    @store.append(link.key, link.val)
   end
 
   def eject!
+    last_used = @store.first
+    @map.delete(last_used.key)
+    last_used.remove
   end
 end
