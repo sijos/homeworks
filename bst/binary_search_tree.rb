174ebc387bb43dc9a8e137377d625c68412f2af9
Scott Snyder <ssny88@gmail.com>
2017-04-18 20:44:03 -0700

    complete find and insert for BST

--- bst/binary_search_tree.rb
+++ bst/binary_search_tree.rb
@@ -1,11 +1,42 @@
+require_relative 'bst_node'
+
 class BinarySearchTree
+  attr_accessor :root
+
   def initialize
+    @root = nil
   end
 
-  def find(el)
+  def find(val, start)
+    return true if val == start.val
+    if val < start.val
+      return false unless start.left
+      find(val, start.left)
+    else
+      return false unless start.right
+      find(val, start.right)
+    end
   end
 
-  def insert(el)
+  def insert(el, start)
+    if !root
+      root = BSTNode.new(el)
+      return
+    end
+    
+    if el < start.val
+      if !start.left
+        start.left = BSTNode.new(el)
+      else
+        insert(el, start.left)
+      end
+    else
+      if !start.right
+        start.right = BSTNode.new(el)
+      else
+        insert(el, start.right)
+      end
+    end
   end
 
   def delete(el)
