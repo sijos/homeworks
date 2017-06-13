e23d69c0e2d14a5e258876010edc2c938a8dfb58
Scott Snyder <ssny88@gmail.com>
2017-06-13 15:36:54 -0700

    complete cats in hats

--- JS_problems/ruby_prep.rb
+++ JS_problems/ruby_prep.rb
@@ -19,3 +19,33 @@ puts compound_words?(["ice", "cream"], "icecream") == true
 puts compound_words?(["cake", "candle", "cup", "wish"], "cupcake") == true
 puts compound_words?(["hello", "bye", "greetings"], "byebye") == false
 puts compound_words?(["bye", "bye", "greetings"], "byebye") == true
+
+# You have 100 cats.
+#
+# Your rules are simple: whenever you visit a cat, you toggle it's hat status (if it
+# already has a hat, you remove it.. if it does not have a hat, you put one on).
+# All of the cats start hat-less. You cycle through 100 rounds of visiting cats.
+# In the 1st round, you visit every cat. In the second round, you visit every other cat.
+# In the nth round, you visit every nth cat.. until the 100th round, in which you only
+# visit the 100th cat.
+#
+# At the end of 100 rounds, which cats have hats?
+
+def cats_in_hats
+  cats = [false] * 100
+
+  (1..100).each do |pass_num|
+    cats.each_index do |cat_idx|
+      cats[cat_idx] = !cats[cat_idx] if (cat_idx + 1) % pass_num == 0
+    end
+  end
+  
+  hatted = []
+  cats.each_with_index do |cat, idx|
+    hatted << idx + 1 if cat
+  end
+  hatted
+end
+
+puts "------Cats in Hats------"
+puts cats_in_hats == [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
