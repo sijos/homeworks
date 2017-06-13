2bd4a343a4c6e2150530033215e14a70b05cbd8c
Scott Snyder <ssny88@gmail.com>
2017-06-13 15:55:40 -0700

    complete even splitters problem

--- JS_problems/ruby_prep.rb
+++ JS_problems/ruby_prep.rb
@@ -49,3 +49,40 @@ end
 
 puts "------Cats in Hats------"
 puts cats_in_hats == [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+
+# Even Splitters
+# ------------------------------------------------------------------------------
+# Return an array of characters on which we can split an input string to produce
+# substrings of the same length.
+
+# Don't count empty strings after the split.
+
+# Here's an example for "banana":
+#
+# "banana".split("a") # => ["b", "n", "n"] (all elements same length)
+# "banana".split("b") # => ["", anana"] (all elements same length - there's only
+# one element "anana" because the empty string doesn't count)
+# "banana".split("n") # => ["ba", "a", "a"] (all elements NOT same length)
+#
+# result: ["b", "a"]
+#
+# ------------------------------------------------------------------------------
+
+def even_splitters(string)
+  result = []
+
+  string.chars.each do |char|
+    splits = string.split(char).reject { |el| el == "" }
+    result << char if splits.all? {|split| split.length == splits.first.length }
+  end
+  
+  result.uniq
+end
+
+puts "-----Even Splitters----"
+puts even_splitters("") == []
+puts even_splitters("t") == ["t"]
+puts even_splitters("jk") == ["j", "k"]
+puts even_splitters("xoxo") == ["x", "o"]
+puts even_splitters("banana") == ["b","a"]
+puts even_splitters("mishmash") == ["m","h"]
