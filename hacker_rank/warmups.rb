677f83fb20ce7ff0e84b435ace1054e463bab5a9
Scott Snyder <ssny88@gmail.com>
2017-06-16 16:58:44 -0700

    complete staircase challenge

--- hacker_rank/warmups.rb
+++ hacker_rank/warmups.rb
@@ -1,3 +1,5 @@
+# Plus / Minus
+
 # stdin: 
 # 6
 # -4 3 -9 0 4 1  
@@ -7,14 +9,35 @@
 # 0.333333
 # 0.166667
 
+# SOLUTION
+# n = gets.strip.to_i
+# arr = gets.strip
+# arr = arr.split(' ').map(&:to_i)
+
+# positive = arr.select{|el| el > 0}
+# negative = arr.select{ |el| el < 0}
+# zero = arr.select{ |el| el == 0}
+
+# puts (positive.count.to_f / n).round(6)
+# puts (negative.count.to_f / n).round(6)
+# puts (zero.count.to_f / n).round(6)
+
+# Staircase
+
+# stdin:
+# 6 
+
+# stdout:
+#      #
+#     ##
+#    ###
+#   ####
+#  #####
+# ######
+
 n = gets.strip.to_i
-arr = gets.strip
-arr = arr.split(' ').map(&:to_i)
 
-positive = arr.select{|el| el > 0}
-negative = arr.select{ |el| el < 0}
-zero = arr.select{ |el| el == 0}
-
-puts (positive.count.to_f / n).round(6)
-puts (negative.count.to_f / n).round(6)
-puts (zero.count.to_f / n).round(6)
+(1..n).each do |stair_num|
+  spaces = n - stair_num
+  puts (" " * spaces) + ("#" * stair_num)
+end
