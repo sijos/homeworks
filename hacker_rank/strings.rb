777beaf0d3f57094994e955a515a6fadbd2f4cf0
Scott Snyder <ssny88@gmail.com>
2017-06-19 15:15:30 -0700

    complete camelcase challenge

--- hacker_rank/strings.rb
+++ hacker_rank/strings.rb
@@ -1,18 +1,29 @@
-def super_reduced_string(s)
-  i = 0
-  chars = s.chars
-  while i < chars.length
-    if chars[i] == chars[i + 1]
-      chars = chars[0...i] + chars[(i + 2)...chars.length]
-      i = 0
-    else
-      i += 1
-    end
-  end
+# def super_reduced_string(s)
+#   i = 0
+#   chars = s.chars
+#   while i < chars.length
+#     if chars[i] == chars[i + 1]
+#       chars = chars[0...i] + chars[(i + 2)...chars.length]
+#       i = 0
+#     else
+#       i += 1
+#     end
+#   end
   
-  chars.empty? ? "Empty String" : chars.join('')
-end
+#   chars.empty? ? "Empty String" : chars.join('')
+# end
+
+# s = gets.strip
+# result = super_reduced_string(s)
+# puts result;
+
 
 s = gets.strip
-result = super_reduced_string(s)
-puts result;
+lower = s.downcase
+i = 0
+counter = 0
+while i < s.length
+  counter += 1 if s[i] != lower[i]
+  i += 1
+end
+puts counter + 1
