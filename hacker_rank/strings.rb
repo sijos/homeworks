1ac1e3bdf7131658f0721e8ef21dd45e7fddb125
Scott Snyder <ssny88@gmail.com>
2017-06-19 17:31:59 -0700

    complete 2 characters challenge

--- hacker_rank/strings.rb
+++ hacker_rank/strings.rb
@@ -18,12 +18,38 @@
 # puts result;
 
 
+# s = gets.strip
+# lower = s.downcase
+# i = 0
+# counter = 0
+# while i < s.length
+#   counter += 1 if s[i] != lower[i]
+#   i += 1
+# end
+# puts counter + 1
+
+len = gets.strip.to_i
 s = gets.strip
-lower = s.downcase
-i = 0
-counter = 0
-while i < s.length
-  counter += 1 if s[i] != lower[i]
-  i += 1
+
+def check(word)
+  for i in (0..word.length - 1)
+    return false if word[i] == word[i+1]
+  end
+  word.chars.uniq.size <= 2
 end
-puts counter + 1
+
+valid_words = []
+letters = s.chars.uniq
+pairs = []
+letters.each_with_index do |el1, i1|
+  letters.each_with_index do |el2, i2|
+    pairs << [el1, el2] unless i1 == i2 || pairs.include?([el2, el1])
+  end
+end
+
+pairs.each do |pair|
+  temp = s.chars.select{|el| pair.include?(el)}.join('')
+  valid_words << temp if check(temp)
+end
+
+puts valid_words.empty? ? 0 : valid_words.max_by(&:length).length
