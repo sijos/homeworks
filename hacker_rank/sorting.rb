b2618203221bf9c3cdf09910d67f2980ad5a06e9
Scott Snyder <ssny88@gmail.com>
2017-06-21 00:20:01 -0700

    complete insertion sort part 1

--- hacker_rank/sorting.rb
+++ hacker_rank/sorting.rb
@@ -1,6 +1,24 @@
-n = gets.strip.to_i
-v = gets.strip.to_i
-arr = gets.strip
-arr = arr.split(' ').map(&:to_i)
+def insertionSort(ar) 
+  new_el = ar.pop
+  idx = ar.length - 1
+  while idx >= 0
+    if ar[idx] > new_el
+      ar[idx + 1] = ar[idx]
+      p ar
+    else
+      ar[idx + 1] = new_el
+      p ar
+      break
+    end
+    idx -= 1
+  end
+  if ar[0] > new_el
+    ar[0] = new_el 
+    p ar
+  end
+end
 
-puts arr.index(v)
+count = gets.to_i
+ar = gets.strip.split.map {|i| i.to_i}
+
+insertionSort( ar )
