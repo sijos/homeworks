649d0afcdd08e726473d12b27e1e1dabccadd751
Scott Snyder <ssny88@gmail.com>
2017-06-22 23:52:15 -0700

    remove stdin stdout line readers

--- hacker_rank/imps.js
+++ hacker_rank/imps.js
@@ -1,35 +1,9 @@
-process.stdin.resume();
-process.stdin.setEncoding('ascii');
-
-var input_stdin = "";
-var input_stdin_array = "";
-var input_currentline = 0;
-
-process.stdin.on('data', function (data) {
-    input_stdin += data;
-});
-
-process.stdin.on('end', function () {
-    input_stdin_array = input_stdin.split("\n");
-    main();    
-});
-
-function readLine() {
-    return input_stdin_array[input_currentline++];
-}
-
-/////////////// ignore above this line ////////////////////
-
 function solve(n, s, d, m){
   var count = 0;
   for (var i = 0; i < s.length; i++) {
     sum = 0;
-    for (var j = 0; j < m; j++) { 
-      sum += s[i + j];
-    }
-    if (sum === d) {
-      count++;
-    }
+    for (var j = 0; j < m; j++) { sum += s[i + j];}
+    if (sum === d) { count++; }
   }
   return count;
 }
