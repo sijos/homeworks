8708cdecb8501b2e4b49616f15722b7f6cb40466
Scott Snyder <ssny88@gmail.com>
2017-06-23 21:57:55 -0700

    complete divisible sum pairs

--- hacker_rank/imps.js
+++ hacker_rank/imps.js
@@ -1,3 +1,4 @@
+// chocolate lengths
 function solve(n, s, d, m){
   var count = 0;
   for (var i = 0; i < s.length; i++) {
@@ -18,5 +19,29 @@ function main() {
     var m = parseInt(d_temp[1]);
     var result = solve(n, s, d, m);
     process.stdout.write(""+result+"\n");
+
+}
+
+// Divisible Sum Pairs
+function divisibleSumPairs(n, k, ar) {
+  let count = 0;
+  for (let i = 0; i < ar.length; i++) {
+    for (let j = i; j < ar.length; j++) {
+      if ((ar[i] + ar[j]) % k === 0) {
+        count++;
+      }
+    }
+  }
+  return count;
+}
+
+function main() {
+    var n_temp = readLine().split(' ');
+    var n = parseInt(n_temp[0]);
+    var k = parseInt(n_temp[1]);
+    ar = readLine().split(' ');
+    ar = ar.map(Number);
+    var result = divisibleSumPairs(n, k, ar);
+    process.stdout.write("" + result + "\n");
 
 }
