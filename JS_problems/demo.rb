8cf89317697695135b8cc345639b8e87019ba120
Scott Snyder <ssny88@gmail.com>
2017-06-26 12:39:09 -0700

    update demo file for recent students

--- JS_problems/demo.rb
+++ JS_problems/demo.rb
@@ -1,48 +1,24 @@
-
-# You have 100 cats.
-#
-# Your rules are simple: whenever you visit a cat, you toggle it's hat status (if it
-# already has a hat, you remove it.. if it does not have a hat, you put one on).
-# All of the cats start hat-less. You cycle through 100 rounds of visiting cats.
-# In the 1st round, you visit every cat. In the second round, you visit every other cat.
-# In the nth round, you visit every nth cat.. until the 100th round, in which you only
-# visit the 100th cat.
-#
-# At the end of 100 rounds, which cats have hats?
-
-# def cats_in_hats
-#   cats = [0] * 100
-#   hatted =  []
-
-#   (1..100).each do |pass_num|
-#     cats.each_index do |cat_idx|
-#       cats[cat_idx] = !cats[cat_idx] if (cat_idx + 1) % pass_num == 0
-#     end
-#   end
-
-#   cats.each_with_index do |cat, idx|
-#     hatted << (idx + 1) if cat
-#   end
-
-#   hatted
-# end
-
-def cats_in_hats
-  cats = {}
-  (1..100).each { |n| cats[n] = 0 }
-  step_num = 1
-
-  until step_num > 100
-    (0..100).step(step_num) do |cat_num|
-      next if cat_num == 0
-      cats[cat_num] = !cats[cat_num]
-    end
-    step_num += 1
-  end
-  
-  cats.select {|num, val| val == true }.keys
+def hipsterfy(sentence)
+  sentence.split(" ").map{|word| hip_word(word)}.join(" ")
 end
 
+def hip_word(word)
+  vowels = %w(a e i o u)
 
-puts "------Cats in Hats------"
-puts cats_in_hats == [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+  # idx = word.length - 1
+  (word.length - 1).downto(0) do |idx|
+    if vowels.include?(word[idx])
+      word.slice!(idx)
+      return word
+    end
+    # idx -= 1
+  end
+
+  word
+end
+
+puts "-------Hipsterfy-------"
+puts hipsterfy("proper") == "propr"
+puts hipsterfy("mstrkrft") == "mstrkrft"
+puts hipsterfy("towel flicker banana") == "towl flickr banan"
+puts hipsterfy("turtle cheeseburger fries") == "turtl cheeseburgr fris"
