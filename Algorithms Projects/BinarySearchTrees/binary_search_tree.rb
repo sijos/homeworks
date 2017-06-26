7de67a4ccdfd9e7540657b40ad7e3f70bc5aa261
Scott Snyder <ssny88@gmail.com>
2017-06-26 11:51:12 -0700

    initial commit algos project skeleton and spec files

---
+++ Algorithms Projects/BinarySearchTrees/binary_search_tree.rb
@@ -0,0 +1,25 @@
+class BinarySearchTree
+  def initialize
+  end
+
+  def find(el)
+  end
+
+  def insert(el)
+  end
+
+  def delete(el)
+  end
+
+  def is_balanced?
+  end
+
+  def in_order_traversal
+  end
+
+  def maximum
+  end
+
+  def depth
+  end 
+end
