56180ab98509be663a632e586cd229b2cdb3ea6c
Scott Snyder <ssny88@gmail.com>
2017-06-27 20:07:50 -0700

    redo demo for JS w1d2

--- JS_problems/demo.rb
+++ JS_problems/demo.rb
@@ -1,24 +1,38 @@
-def hipsterfy(sentence)
-  sentence.split(" ").map{|word| hip_word(word)}.join(" ")
-end
+# Compound Words
 
-def hip_word(word)
-  vowels = %w(a e i o u)
+# Define a method, #compound_words?(word_list, target), that accepts 2 arguments:
+# an array of words and a target, compound word. Check to see if any two words
+# in the list can be combined to make the compound word. Don't double-use words.
 
-  # idx = word.length - 1
-  (word.length - 1).downto(0) do |idx|
-    if vowels.include?(word[idx])
-      word.slice!(idx)
-      return word
-    end
-    # idx -= 1
+
+# def compound_words?(array, target)
+#   array.each_with_index do |el1, idx1|
+#     array.each_with_index do |el2, idx2|
+#       return true if el1 + el2 == target && idx1 != idx2
+#     end
+#   end
+#   false
+# end
+
+# puts "------Compound Words------"
+# puts compound_words?(["ice", "cream"], "icecream") == true
+# puts compound_words?(["cake", "candle", "cup", "wish"], "cupcake") == true
+# puts compound_words?(["hello", "bye", "greetings"], "byebye") == false
+# puts compound_words?(["bye", "bye", "greetings"], "byebye") == true
+
+ # Define a method that, given a sentence, returns a hash of each of the words as
+ # keys with their lengths as values. Assume the argument lacks punctuation.
+def word_lengths(str)
+  hash = {}
+
+  str.split(' ').each do |word|
+    hash[word] = word.length
   end
-
-  word
+  hash
 end
 
-puts "-------Hipsterfy-------"
-puts hipsterfy("proper") == "propr"
-puts hipsterfy("mstrkrft") == "mstrkrft"
-puts hipsterfy("towel flicker banana") == "towl flickr banan"
-puts hipsterfy("turtle cheeseburger fries") == "turtl cheeseburgr fris"
+test_sent = "this is a test sentence"
+new_hash = word_lengths(test_sent)
+p new_hash.keys
+p new_hash.values
+p new_hash.select{|key, value| value >= 3}
