28ad1637a225b4ee961a4c515de3a8c164e1039a
Scott Snyder <ssny88@gmail.com>
2017-06-29 14:33:34 -0700

    implement hashing functions

--- Algorithms Projects/HashmapLRUCache/lib/p02_hashing.rb
+++ Algorithms Projects/HashmapLRUCache/lib/p02_hashing.rb
@@ -4,18 +4,22 @@ end
 
 class Array
   def hash
+    magic_num = 42
+    self.each_with_index do |el, idx|
+      magic_num += el.hash * (idx + 1)
+    end
+    magic_num
   end
 end
 
 class String
   def hash
+    self.chars.map(&:ord).hash
   end
 end
 
 class Hash
-  # This returns 0 because rspec will break if it returns nil
-  # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    self.keys.sort.concat(self.values.sort).hash
   end
 end
