0f76962a7ef9359decb5af2040a4372d0787aea5
Scott Snyder <ssny88@gmail.com>
2017-06-29 14:25:14 -0700

    complete resizing int set class

--- Algorithms Projects/HashmapLRUCache/lib/p01_int_set.rb
+++ Algorithms Projects/HashmapLRUCache/lib/p01_int_set.rb
@@ -64,18 +64,24 @@ class ResizingIntSet
   end
 
   def insert(num)
+    resize! if @count == num_buckets
+    self[num] << num
+    @count += 1
   end
 
   def remove(num)
+    self[num].delete(num)
+    @count -= 1
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
-    # optional but useful; return the bucket corresponding to `num`
+    @store[num % num_buckets]
   end
 
   def num_buckets
@@ -83,5 +89,11 @@ class ResizingIntSet
   end
 
   def resize!
+    new_buckets = num_buckets * 2
+    new_store = Array.new(new_buckets) { Array.new }
+    @store.each do |bucket|
+      bucket.each { |n| new_store[n % new_buckets] << n }
+    end
+    @store = new_store
   end
 end
