c455a9acb74911e4d7a17880ee7e7c874fbe114f
Scott Snyder <ssny88@gmail.com>
2017-07-06 11:35:33 -0700

    complete hw step 2 demo

--- JS_problems/demo.rb
+++ JS_problems/demo.rb
@@ -1,38 +1,44 @@
-# Compound Words
+# Number Counting Sequence
+# ------------------------------------------------------------------------------
+# The number counting sequence is a sequence of number strings that begins with:
+# ["1", "11", "21", "1211", "111221", ...]
+#
+# "1" is counted as "one 1" or "11", followed by
+# "11" which is counted as "two 1s" or "21", followed by
+# "21" which is counted as "one 2 and one 1" or "1211", and so on.
+#
+# It helps to "say it out loud" to describe the item in the sequence.
+#
+# Given an integer n, generate the n-th element in the number counting sequence.
 
-# Define a method, #compound_words?(word_list, target), that accepts 2 arguments:
-# an array of words and a target, compound word. Check to see if any two words
-# in the list can be combined to make the compound word. Don't double-use words.
-
-
-# def compound_words?(array, target)
-#   array.each_with_index do |el1, idx1|
-#     array.each_with_index do |el2, idx2|
-#       return true if el1 + el2 == target && idx1 != idx2
-#     end
-#   end
-#   false
-# end
-
-# puts "------Compound Words------"
-# puts compound_words?(["ice", "cream"], "icecream") == true
-# puts compound_words?(["cake", "candle", "cup", "wish"], "cupcake") == true
-# puts compound_words?(["hello", "bye", "greetings"], "byebye") == false
-# puts compound_words?(["bye", "bye", "greetings"], "byebye") == true
-
- # Define a method that, given a sentence, returns a hash of each of the words as
- # keys with their lengths as values. Assume the argument lacks punctuation.
-def word_lengths(str)
-  hash = {}
-
-  str.split(' ').each do |word|
-    hash[word] = word.length
-  end
-  hash
+def number_counting_seq(n)
+  return "" if n <= 0
+  sequence = ["1"]
+  sequence << next_seq(sequence.last) until sequence.length == n
+  sequence.last
 end
 
-test_sent = "this is a test sentence"
-new_hash = word_lengths(test_sent)
-p new_hash.keys
-p new_hash.values
-p new_hash.select{|key, value| value >= 3}
+def next_seq(str)
+  result = ""
+  i = 0
+  repeats = 1
+  while i < str.length
+    if str[i] == str[i + 1]
+      repeats += 1
+    else
+      result << repeats.to_s + str[i]
+      repeats = 1
+    end
+    i += 1
+  end
+  result
+end
+
+puts "-------Number Counting Sequence-------"
+puts number_counting_seq(0) == ""
+puts number_counting_seq(1) == "1"
+puts number_counting_seq(2) == "11" # one 1 above
+puts number_counting_seq(3) == "21" # two 1s above
+puts number_counting_seq(5) == "111221" #the last sequence was one 1, one 2, and two 1s: "1211"
+puts number_counting_seq(8) == "1113213211"
+puts number_counting_seq(10) == "13211311123113112211"
