4ed3afc079839fcb7fa354b0034a676a5ae57001
Scott Snyder <ssny88@gmail.com>
2017-07-07 13:44:41 -0700

    initial commit step 2 assessment 5

---
+++ JS_problems/assessments/s2a5.rb
@@ -0,0 +1,66 @@
+### Missing Numbers
+#
+# Given an array of unique integers ordered from least to greatest, write
+# a method that returns an array of the integers that are needed to
+# fill in the consecutive set.
+
+def missing_numbers(nums)
+
+end
+
+puts "-------Missing Numbers-------"
+puts missing_numbers([1, 3]) == [2]
+puts missing_numbers([2, 3, 7]) == [4, 5, 6]
+puts missing_numbers([-5, -1, 0, 3]) == [-4, -3, -2, 1, 2]
+puts missing_numbers([0, 5]) == [1, 2, 3, 4]
+
+
+
+### Titleize
+#
+# Write a method that capitalizes each word in a string like a book title.
+# Do not capitalize words like 'a', 'and', 'of', 'over' or 'the'.
+
+def titleize(title)
+
+end
+
+puts "-------Titleize-------"
+puts titleize("basketball") == "Basketball"
+puts titleize("stephen curry") == "Stephen Curry"
+puts titleize("war and peace") == "War and Peace"
+puts titleize("the bridge over the river kwai") == "The Bridge over the River Kwai"
+
+
+
+# Unique in Order
+#
+# Implement the function unique_in_order which takes a string and
+# returns an array of letters without any elements with the same value next
+# to each other. Preserve the original order of elements.
+
+def unique_in_order(string)
+
+end
+
+puts "-------Unique in Order-------"
+puts unique_in_order('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
+puts unique_in_order('BBBAAAACCDAABBB') == ['B', 'A', 'C', 'D', 'A', 'B']
+puts unique_in_order('ABBCcAD') == ['A', 'B', 'C', 'c', 'A', 'D']
+puts unique_in_order('aAa') == ['a', 'A', 'a']
+
+
+
+# Substrings
+#
+# Return an array of all the substrings for a given string.
+# Be sure that the returned array is sorted.
+
+def substrings(string)
+
+end
+
+puts "-------Substrings-------"
+puts substrings("") == []
+puts substrings("123") == ["1", "12", "123", "2", "23", "3"]
+puts substrings("ruby") == ["b", "by", "r", "ru", "rub", "ruby", "u", "ub", "uby", "y"]
