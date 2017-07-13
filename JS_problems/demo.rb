3d16fe0d6da0a1acf6d3fa378674f0908e5a8cee
Scott Snyder <ssny88@gmail.com>
2017-07-12 19:38:58 -0700

    complete step 1 w1d3 demo

--- JS_problems/demo.rb
+++ JS_problems/demo.rb
@@ -1,8 +1,24 @@
 def every_other_letter(string)
-  # your code here
+  new_str = ""
+  idx = 0
+  while idx < string.length
+    new_str << string[idx]if idx.even?
+    idx += 1
+  end
+  return new_str
+end
+
+def every_other_letter(string)
+  new_str = ""
+  idx = 0
+  until idx == string.length
+    new_str << string[idx] if idx.even?
+    idx += 1
+  end
+  new_str
 end
 
 puts "------Every Other Letter------"
-puts every_other_letter("abcde") == "ace"
+puts every_other_letter("abcde") #== "ace"
 puts every_other_letter("i heart ruby") == "ihatrb"
-puts every_other_letter("an apple a day...") == "a pl  a.."
+puts every_other_letter("an apple a day...") == "a pl  a.." 
