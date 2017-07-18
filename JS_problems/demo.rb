84115970794ce9e13e4a9e7987cdc8f7c977c105
Scott Snyder <ssny88@gmail.com>
2017-07-17 18:51:19 -0700

    prep demo for s2 w2d1

--- JS_problems/demo.rb
+++ JS_problems/demo.rb
@@ -1,26 +1,22 @@
-def every_other_letter(string)
-  new_str = ""
-  idx = 0
-  while idx < string.length
-    new_str << string[idx]if idx.even?
-    idx += 1
-  end
-  new_str
+# Hipsterfy
+
+# Define a method, #hipsterfy, that accepts a sentence (string) as an argument.
+# The method should return a new string with the last vowel removed from each word.
+
+# 'y' is not a vowel. If a word has no vowels, don't change it. You may assume all
+# letters are lower-case.
+
+# ex. hipsterfy("towel flicker banana") => "towl flickr banan"
+
+def hipsterfy(sentence)
+  #
+  # your code goes here
+  #
 end
 
-def every_other_letter(string)
-  new_str = ""
-  idx = 0
-  until idx == string.length
-    if idx.even?
-      new_str << string[idx]
-    end
-    idx += 1
-  end
-  new_str
-end
 
-puts "------Every Other Letter------"
-puts every_other_letter("abcde") #== "ace"
-puts every_other_letter("i heart ruby") == "ihatrb"
-puts every_other_letter("an apple a day...") == "a pl  a.." 
+puts "-------Hipsterfy-------"
+puts hipsterfy("proper") == "propr"
+puts hipsterfy("mstrkrft") == "mstrkrft"
+puts hipsterfy("towel flicker banana") == "towl flickr banan"
+puts hipsterfy("turtle cheeseburger fries") == "turtl cheeseburgr fris"
