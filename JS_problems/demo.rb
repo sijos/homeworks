f086fbffe9104495414cca3ead1bd9fc26a21981
Scott Snyder <ssny88@gmail.com>
2017-07-25 19:03:36 -0700

    complete three digit format demo

--- JS_problems/demo.rb
+++ JS_problems/demo.rb
@@ -5,7 +5,19 @@
 # n, but with leading zeros such that the string is always 3 characters long.
 
 def three_digit_format(n)
-  # your code here
+  my_num = n.to_s
+  
+  if n < 10
+    my_num = "00" + my_num
+  elsif n < 100
+    my_num = "0" + my_num
+  end
+
+  # while my_num.length < 3
+  #   my_num = "0" + my_num
+  # end
+
+  my_num
 end
 
 puts "------Three Digit Format------"
