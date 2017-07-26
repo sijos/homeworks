e7f6d4dce83feab902402da99e476bbf9d013647
Scott Snyder <ssny88@gmail.com>
2017-07-25 18:21:27 -0700

    prep three digit format demo

--- JS_problems/demo.rb
+++ JS_problems/demo.rb
@@ -1,38 +1,14 @@
+# Three Digit Format
 
-# Even Splitters
-# ------------------------------------------------------------------------------
-# Return an array of characters on which we can split an input string to produce
-# substrings of the same length.
+# Define a method, #three_digit_format(n), that accepts an integer, n, as an
+# argument. Assume that n < 1000. Your method should return a string version of
+# n, but with leading zeros such that the string is always 3 characters long.
 
-# Don't count empty strings after the split.
-
-# Here's an example for "banana":
-#
-# "banana".split("a") # => ["b", "n", "n"] (all elements same length)
-# "banana".split("b") # => ["", anana"] (all elements same length - there's only
-# one element "anana" because the empty string doesn't count)
-# "banana".split("n") # => ["ba", "a", "a"] (all elements NOT same length)
-#
-# result: ["b", "a"]
-#
-# ------------------------------------------------------------------------------
-
-def even_splitters(string)
-  result = []
-
-  string.each_char do |ch|
-    splits = string.split(ch).reject{ |c| c == "" }
-    result << ch if splits.all?{ |str| str.length == splits.first.length }
-    # result << ch if splits.count{ |s| s.length == splits.first.length } == splits.length
-  end
-
-  result.uniq
+def three_digit_format(n)
+  # your code here
 end
 
-puts "-----Even Splitters----"
-puts even_splitters("") == []
-puts even_splitters("t") == ["t"]
-puts even_splitters("jk") == ["j", "k"]
-puts even_splitters("xoxo") == ["x", "o"]
-puts even_splitters("banana") == ["b","a"]
-puts even_splitters("mishmash") == ["m","h"]
+puts "------Three Digit Format------"
+puts three_digit_format(100) == "100"
+puts three_digit_format(15) == "015"
+puts three_digit_format(6) == "006"
